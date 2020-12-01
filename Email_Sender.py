55caa3102ea6fd336662ad588cad3c271a2db4b4
KiranKumar <balakirankumar999@gmail.com>
2020-12-01 06:47:18 +0530

    Comments added

--- Email_Sender.py
+++ Email_Sender.py
@@ -6,13 +6,13 @@
 
 import os
 import smtplib
-import imghdr
 from email.message import EmailMessage
 import requests
 import lxml
 from bs4 import BeautifulSoup
-from pprint import pprint
 import random
+
+# take passwords from other file or from environ variables
 EMAIL_ADDRESS=os.environ.get('Email_mail')
 EMAIL_PASSWORD=os.environ.get('Email_pass')
 
@@ -20,17 +20,23 @@ EMAIL_PASSWORD=os.environ.get('Email_pass')
 url='http://quotes.toscrape.com/tag/'
 type_=["love","inspirational","life","humor","books","reading","friendship","friends","truth","simile",]
 url_final=url+random.choice(type_)+"/"
-#making request
+
+#making request to url
 r=requests.get(url_final)
+# getting into lxml form
 soup=BeautifulSoup(r.text,'lxml')
+
 #getting required data
 quotes=soup.find_all('span',class_='text')
 author=soup.find_all('small',class_='author')
 tags=soup.find_all('div',class_='tags')
+
+#To select a random quote from the list all quotes are appended to this list
 quotes_l=[]
 author_l=[]
 tags_l=[]
-# getting text from html code
+
+# getting text from lxml code
 for i in range(len(quotes)):
     quotes_l.append(quotes[i].text)
     author_l.append(author[i].text)
@@ -41,11 +47,12 @@ for i in range(len(quotes)):
     tags_l.append(t.lstrip(", "))
 len_lists=len(quotes_l)
 numb=random.randrange(0,len_lists)-1
-#msg composing
-# to
+
+#msg composing to
 contacts = ["Reciever1@gmail.com","Reciever2@gmail.com","so_on@gmail.com"]
 subject= "Good Morning "
 content=f"Quote: {quotes_l[numb]} \nAuthor: {author_l[numb]}. \nTags: {tags_l[numb]}."
+
 msg = EmailMessage()
 msg['Subject'] = subject
 msg['From'] = EMAIL_ADDRESS
